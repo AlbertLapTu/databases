0a049b6f5ed8dd89db64ab58fb1ea920b5655c9a
Hack Reactor Student <student@hackreactor.com>
2018-12-05 22:23:13 -0800

    updated sql syntax

--- server/schema.sql
+++ server/schema.sql
@@ -1,28 +1,30 @@
+CREATE DATABASE chat;         
+
 USE chat;
 
+CREATE TABLE rooms (
+  id int(5) auto_increment,
+  roomname varchar(20),
+  UNIQUE KEY roomname(roomname),
+  primary key (id)
+);
+
+CREATE TABLE users (
+  id int(5) auto_increment,
+  username varchar(20),
+  UNIQUE KEY username(username),
+  primary key (id)
+);
+
 CREATE TABLE messages (
-  username varchar(255) NOT NULL PRIMARY KEY,
-  id int NOT NULL,
-  roomname varchar(255) NOT NULL,
-  text varchar(255)
-  FOREIGN KEY (username) REFERENCES username(name),
-  FOREIGN KEY (roomname) REFERENCES roomname(roomname),
-  FOREIGN KEY (text) REFERENCES text(text)
-);
+  id int(5) auto_increment,
+  message varchar(180),
+  user_id int(5),
+  room_id int(5),
 
-CREATE TABLE username (
-  name varchar(255) NOT NULL PRIMARY KEY,
-  id int NOT NULL-- CREATE DATABASE chat;
-);
-
-CREATE TABLE roomname (
-  roomname varchar(255) NOT NULL PRIMARY KEY,
-  id int NOT NULL
-);
-
-CREATE TABLE text (
-  text varchar(255) NOT NULL PRIMARY KEY,
-  id INT NOT NULL
+  foreign key (user_id) references users(id),
+  foreign key (room_id) references rooms(id),
+  primary key (id)
 );
 
 /*  Execute this file from the command line by typing:
