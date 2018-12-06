f188e3489742391e1d3d3715b46aad0d76dd2267
Hack Reactor Student <student@hackreactor.com>
2018-12-05 20:05:27 -0800

    Completed first few schema commands

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,29 @@
-CREATE DATABASE chat;
-
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  username varchar(255) NOT NULL PRIMARY KEY,
+  id int NOT NULL,
+  roomname varchar(255) NOT NULL,
+  text varchar(255)
+  -- FOREIGN KEY (username) REFERENCES username(name),
+  -- FOREIGN KEY (roomname) REFERENCES roomname(roomname),
+  -- FOREIGN KEY (text) REFERENCES text(text)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE username (
+  name varchar(255) NOT NULL PRIMARY KEY,
+  id int NOT NULL-- CREATE DATABASE chat;
+);
 
+CREATE TABLE roomname (
+  roomname varchar(255) NOT NULL PRIMARY KEY,
+  id int NOT NULL
+);
 
-
+CREATE TABLE text (
+  text varchar(255) NOT NULL PRIMARY KEY,
+  id INT NOT NULL
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
