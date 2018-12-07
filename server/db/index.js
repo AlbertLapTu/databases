1f296d93586177eef8db3118635883f2eb8bedda
Hack Reactor Student <student@hackreactor.com>
2018-12-06 20:04:11 -0800

    Added in DB connection

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,12 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'student',
+    password: 'student',
+    database: chat
+})
 
+console.log('hi');
+connection.end();
