1d6a6d7e455bd8ff264a4f4c701ab4fe55aa636f
Hack Reactor Student <student@hackreactor.com>
2018-12-06 19:17:14 -0800

    Added in potential 'GET' response

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,25 +4,39 @@ module.exports = {
   messages: {
     get: function (req, res) {
       models.messages.get(function(err, results) { //ref the models file, the messages obj within it, and the get key 
-        if (err) {} //If the response is bad, do nothing 
-        res.json(results);  //else, if the response is good, run the callback
+        if (err) {
+          console.log('This is an error!');
+        } //If the response is bad, do nothing 
+        JSON.stringify(res.body)   //else, if the response is good, run the callback
       });
     }, 
     post: function (req, res) {
-      var params = [];
+
     }
   },
   users: {
     // Ditto as above
     get: function (req, res) {
-      if (request.uri !== '/classes/users') {
-
-      }
-    },
-    post: function (req, res) {}
+      models.users.get(function(err, results) { //ref the models file, the messages obj within it, and the get key 
+        if (err) {
+          console.log('This is an error');
+        } //If the response is bad, do nothing 
+        JSON.stringify(res.body)
+    });
+  },
+    post: function (req, res) {
+      // if (err) {
+      //   console.log('This is an error');
+      // }
+      // models.users.post(function(err, result) {
+      //   if (err) {
+      //     console.log('This is an error');
+      //   }
+        //NEed to do something here, once the work on the models has finished)
+    }
   }
 }
-}
+
 
 
 /*
