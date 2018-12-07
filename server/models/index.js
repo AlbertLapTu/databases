fae36a7495b2b7a898e064cb5e29e44bbb6e127b
Hack Reactor Student <student@hackreactor.com>
2018-12-06 20:04:32 -0800

    Added POST/GET functions for models

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,45 @@
 var db = require('../db');
 
+
+//Here's where we want to query and retrieve data from our database. (Models)
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      db.query('SELECT * FROM messages', function(err, res) {
+        if (err) {
+          console.log('You have an error');
+        } else {
+          callback(res.body);
+        }
+      })
+    }, // a function which produces all the messages
+    post: function (messageObject) {
+      //will need to use a question mark in here in order to pass message in easier
+      // without escaping within a string
+      db.query('INSERT INTO messages (message) VALUES( ? )'), [`${messageObject.message}`], function(err, res) {
+        if (err) {
+          console.log('You have an error');
+        } else {
+          callback(res.body);
+        }
+      }
+    } 
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    //We pass in a callback as a GET request is async. 
+    get: function (callback) {
+      //Query the database using db.query('SQL COMMAND', queryArgs..?, callback(err, results));
+      db.query('SELECT users FROM messages', [], function(err,res){
+        if (err) {
+          console.log('You have an error');
+        } else {
+          callback(res.body);
+        }
+      })
+    },
+    post: function (messageObject) {}
   }
 };
 
